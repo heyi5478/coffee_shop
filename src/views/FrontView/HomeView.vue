a1039101402e352b64b8dca18a2a960c90d36e25
YI-JYUN HE <jobs.steve54@gmail.com>
2024-04-12 16:52:21 +0800

    [ fix ] 修改btn 背景色

--- src/views/FrontView/HomeView.vue
+++ src/views/FrontView/HomeView.vue
@@ -13,7 +13,7 @@
           <p>從原產地的一株咖啡樹，最終成為您手中的一杯咖啡；我們堅持採購並且烘焙最高品質的咖啡，這是我們一直努力的基本原則；最初的十英呎到最後十英呎的珍貴體驗，為咖啡的故事做了最佳的註解，同時也塑造出咖啡家族的獨特風味及口感特性，閱讀咖啡的故事，可以讓您更瞭解咖啡，豐富您的咖啡體驗。
           </p>
           <br>
-          <RouterLink to="/products" class="text-decoration-none">去購物</RouterLink>
+          <RouterLink to="/products" class="text-decoration-none btn">去購物</RouterLink>
 
         </div>
       </div>
