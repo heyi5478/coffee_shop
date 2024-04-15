4ecf52c7eb20a690cce45d7d99e1063b6ac10bef
YI-JYUN HE <jobs.steve54@gmail.com>
2024-04-15 15:27:53 +0800

    [ fix ] 修改 checkout 表單大小

--- src/views/FrontView/CheckoutView.vue
+++ src/views/FrontView/CheckoutView.vue
@@ -2,126 +2,129 @@
   <div class="container">
     <!-- 有商品頁面 -->
     <template v-if="cart?.carts?.length">
-    <VueLoading :active="isLoading" :z-index="1060" />
-    <div class="mt-4">
+      <VueLoading :active="isLoading" :z-index="1060" />
+      <div class="mt-4 row justify-content-center">
      <!-- 訂單進度 -->
-      <div class="mb-4">
-        <OrderTimeLine :step="step" />
-      </div>
-     <!-- 購物車列表 -->
-      <div class="text-end">
-        <button class="btn btn-outline-danger" type="button"
-         @click="deleteAllCarts">
-         清空購物車
-        </button>
-      </div>
-      <table class="table align-middle">
-        <thead>
-          <tr>
-            <th></th>
-            <th>品名</th>
-            <th style="width: 110px">數量</th>
-            <th>單價</th>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-if="cart.carts">
-            <tr v-for="item in cart.carts" :key="item.id">
-              <td>
-                <button type="button" class="btn btn-outline-danger btn-sm"
-                 :disabled="status.loadingItem === item.id"
-                  @click="removeCartItem(item.id)">
-                 <i class="bi bi-x"></i>
-                </button>
-              </td>
-              <td>
-                {{ item.product.title }}
-                <div class="text-success" v-if="item.coupon">已套用優惠券</div>
-              </td>
-              <td>
-                <div class="input-group input-group-sm">
-                  <input type="number" class="form-control" min="1"
-                   v-model.number="item.qty" @blur="updateCart(item)" />
-                  <div class="input-group-text">/ {{ item.product.unit }}</div>
-                </div>
-              </td>
-              <td class="text-end">
-                <small v-if="cart.final_total !== cart.total" class="text-success">折扣價：</small>
-                 {{ $filters.currency(item.final_total) }}
-              </td>
-            </tr>
-          </template>
-        </tbody>
-        <tfoot>
-          <tr>
-             <td colspan="3" class="text-end">總計</td>
-             <td class="text-end">{{ $filters.currency(cart.total) }}</td>
-          </tr>
-          <tr v-if="cart.final_total !== cart.total">
-            <td colspan="3" class="text-end text-success">折扣價</td>
-            <td class="text-end text-success">
-             {{ $filters.currency(cart.final_total) }}
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-      <!-- 優惠卷 -->
-      <!-- <div class="input-group mb-3 input-group-sm">
-        <input type="text" class="form-control" v-model="coupon_code" placeholder="請輸入優惠碼" />
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button" @click="addCouponCode">
-            套用優惠碼
-          </button>
+        <div class="mb-4">
+          <OrderTimeLine :step="step" />
         </div>
-      </div> -->
-    </div>
-
-   <div class="my-5 row justify-content-center">
-    <p class="text-center text-danger">星號欄位必填</p>
-    <VeeForm ref="form" class="col-md-6" v-slot="{ errors }" @submit="createOrder">
-      <div class="mb-3">
-        <label for="email" class="form-label">Email<span class="text-danger">*</span></label>
-        <VeeField id="email" name="email" type="email"
-         class="form-control" :class="{ 'is-invalid': errors['email'] }"
-         placeholder="請輸入 Email" rules="email|required" v-model="form.user.email" />
-        <ErrorMessage name="email" class="invalid-feedback" />
+     <!-- 購物車列表 -->
+        <div class="col-md-8">
+          <div class="text-end">
+            <button class="btn btn-outline-danger" type="button"
+             @click="deleteAllCarts">
+             清空購物車
+            </button>
+          </div>
+          <table class="table align-middle">
+            <thead>
+              <tr>
+                <th></th>
+                <th>品名</th>
+                <th style="width: 110px">數量</th>
+                <th>單價</th>
+              </tr>
+            </thead>
+            <tbody>
+              <template v-if="cart.carts">
+                <tr v-for="item in cart.carts" :key="item.id">
+                  <td>
+                    <button type="button" class="btn btn-outline-danger btn-sm"
+                     :disabled="status.loadingItem === item.id"
+                      @click="removeCartItem(item.id)">
+                     <i class="bi bi-x"></i>
+                    </button>
+                  </td>
+                  <td>
+                    {{ item.product.title }}
+                    <div class="text-success" v-if="item.coupon">已套用優惠券</div>
+                  </td>
+                  <td>
+                    <div class="input-group input-group-sm">
+                      <input type="number" class="form-control" min="1"
+                       v-model.number="item.qty" @blur="updateCart(item)" />
+                      <div class="input-group-text">/ {{ item.product.unit }}</div>
+                    </div>
+                  </td>
+                  <td class="text-end">
+                    <small v-if="cart.final_total !== cart.total" class="text-success">折扣價：</small>
+                     {{ $filters.currency(item.final_total) }}
+                  </td>
+                </tr>
+              </template>
+            </tbody>
+            <tfoot>
+              <tr>
+                 <td colspan="3" class="text-end">總計</td>
+                 <td class="text-end">{{ $filters.currency(cart.total) }}</td>
+              </tr>
+              <tr v-if="cart.final_total !== cart.total">
+                <td colspan="3" class="text-end text-success">折扣價</td>
+                <td class="text-end text-success">
+                 {{ $filters.currency(cart.final_total) }}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      <!-- 優惠卷 -->
+        <!-- <div class="input-group mb-3 input-group-sm">
+          <input type="text" class="form-control" v-model="coupon_code" placeholder="請輸入優惠碼" />
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" @click="addCouponCode">
+              套用優惠碼
+            </button>
+          </div>
+        </div> -->
       </div>
 
-      <div class="mb-3">
-        <label for="name" class="form-label">收件人姓名<span class="text-danger">*</span></label>
-        <VeeField id="name" name="姓名" type="text"
-         class="form-control" :class="{ 'is-invalid': errors['姓名'] }"
-         placeholder="請輸入姓名" rules="required" v-model="form.user.name" />
-        <ErrorMessage name="姓名" class="invalid-feedback" />
-      </div>
+      <div class="my-5 row justify-content-center">
+        <p class="text-center text-danger">星號欄位必填</p>
+        <VeeForm ref="form" class="col-md-8" v-slot="{ errors }" @submit="createOrder">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email<span class="text-danger">*</span></label>
+            <VeeField id="email" name="email" type="email"
+             class="form-control" :class="{ 'is-invalid': errors['email'] }"
+             placeholder="請輸入 Email" rules="email|required" v-model="form.user.email" />
+            <ErrorMessage name="email" class="invalid-feedback" />
+          </div>
 
-      <div class="mb-3">
-        <label for="tel" class="form-label">收件人電話<span class="text-danger">*</span></label>
-        <VeeField id="tel" name="電話" type="tel"
-         class="form-control" :class="{ 'is-invalid': errors['電話'] }"
-         placeholder="請輸入電話" rules="required|min:8|max:10" v-model="form.user.tel" />
-        <ErrorMessage name="電話" class="invalid-feedback" />
-      </div>
+          <div class="mb-3">
+            <label for="name" class="form-label">收件人姓名<span class="text-danger">*</span></label>
+            <VeeField id="name" name="姓名" type="text"
+             class="form-control" :class="{ 'is-invalid': errors['姓名'] }"
+             placeholder="請輸入姓名" rules="required" v-model="form.user.name" />
+            <ErrorMessage name="姓名" class="invalid-feedback" />
+          </div>
 
-      <div class="mb-3">
-        <label for="address" class="form-label">收件人地址<span class="text-danger">*</span></label>
-        <VeeField id="address" name="地址" type="text"
-         class="form-control" :class="{ 'is-invalid': errors['地址'] }"
-         placeholder="請輸入地址" rules="required" v-model="form.user.address" />
-        <ErrorMessage name="地址" class="invalid-feedback" />
-      </div>
+          <div class="mb-3">
+            <label for="tel" class="form-label">收件人電話<span class="text-danger">*</span></label>
+            <VeeField id="tel" name="電話" type="tel"
+             class="form-control" :class="{ 'is-invalid': errors['電話'] }"
+             placeholder="請輸入電話" rules="required|min:8|max:10" v-model="form.user.tel" />
+            <ErrorMessage name="電話" class="invalid-feedback" />
+          </div>
 
-      <div class="mb-3">
-        <label for="message" class="form-label">備註</label>
-        <textarea name="" id="message" class="form-control"
-         cols="30" rows="10" v-model="form.message"></textarea>
+          <div class="mb-3">
+            <label for="address" class="form-label">收件人地址<span class="text-danger">*</span></label>
+            <VeeField id="address" name="地址" type="text"
+             class="form-control" :class="{ 'is-invalid': errors['地址'] }"
+             placeholder="請輸入地址" rules="required" v-model="form.user.address" />
+            <ErrorMessage name="地址" class="invalid-feedback" />
+          </div>
+
+          <div class="mb-3">
+            <label for="message" class="form-label">備註</label>
+            <textarea name="" id="message" class="form-control"
+            cols="30" rows="10" v-model="form.message"></textarea>
+          </div>
+          <div class="text-end">
+            <button type="submit" class="btn btn-primary">送出訂單</button>
+          </div>
+        </VeeForm>
       </div>
-      <div class="text-end">
-        <button type="submit" class="btn btn-primary">送出訂單</button>
-      </div>
-    </VeeForm>
-   </div>
-   </template>
+    </template>
+
    <!-- 無商品頁面 -->
    <template v-else>
     <div class="py-10 text-center">
