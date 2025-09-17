c23f50573b2f357757a2c4a58e161fb54f92b2b6
Steve Ho <jobs.steve54@gmail.com>
2025-09-17 08:57:56 +0800

    [ chore ] 移除不必要的 ESLint 設定，簡化 FrontView.vue 檔案

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,6 +1,3 @@
-<!-- eslint-disable import/extensions -->
-<!-- eslint-disable comma-dangle -->
-<!-- eslint-disable no-trailing-spaces -->
 <!-- eslint-disable max-len -->
 <template>
 <!-- preloader -->
